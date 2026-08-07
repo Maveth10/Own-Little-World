89273fc168bc8132df4dd2ca6faa374930a9b2da
Maveth10 <sagajdakow@gmail.com>
2026-08-07 18:05:05 +0200

    a

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    // Wskazujemy Next.js, aby nie pakował tych bibliotek do bundla
+    serverExternalPackages: ['@xenova/transformers', 'onnxruntime-node'],
+    webpack: (config) => {
+      // Ignorujemy natywny moduł binarny onnxruntime-node
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'onnxruntime-node$': false,
+      };
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
