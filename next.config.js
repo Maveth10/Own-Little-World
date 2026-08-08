91001a79854c05eba5678d09d11ad3d44a2d0cc1
Maveth10 <sagajdakow@gmail.com>
2026-08-08 16:37:48 +0200

    1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Wskazujemy Next.js, aby nie pakował tych bibliotek do bundla
-    serverExternalPackages: ['@xenova/transformers', 'onnxruntime-node'],
-    webpack: (config) => {
-      // Ignorujemy natywny moduł binarny onnxruntime-node
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        'onnxruntime-node$': false,
-      };
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
+  experimental: {
+    serverComponentsExternalPackages: ['groq-sdk'],
+  },
+};
+
+module.exports = nextConfig;
